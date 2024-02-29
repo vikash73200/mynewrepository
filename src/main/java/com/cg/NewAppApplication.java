6ef4a160d76f7e20505a3d6e6e51b6b26ef4faf7
vikash73200 <vikashojha732000@gmail.com>
2024-02-29 23:21:52 +0530

    my initial commit

---
+++ src/main/java/com/cg/NewAppApplication.java
@@ -0,0 +1,13 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewAppApplication.class, args);
+	}
+
+}
